0e270a2b2b0d1964a6527793dfc7e37cc2f0ed2e
jumang4423 <catontheskate4423@gmail.com>
2025-01-02 02:01:03 +0900

    unko

--- src/driver.py
+++ src/driver.py
@@ -8,7 +8,7 @@ import platform
 
 def get_driver():
     options = Options()
-    # options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument("--disable-gpu")  # Windows向けの設定
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
